d8675d139d2d634018b39756a42a3ec881d2282d
GwenkH <98966055+GwenkH@users.noreply.github.com>
2022-09-12 20:23:31 -0400

    Add files via upload
    Senior CCC '22. Disappointing result. Hope to do better next year.

---
+++ CCC '22.py
@@ -0,0 +1,26 @@
+#Question 1
+import sys
+input = input.stdin.readline
+
+num = int(input())
+
+total = 0
+# Running sum
+sum = 5
+
+# Divisible by 4
+if num % 4 == 0:
+    total += 1
+
+# Divisible by 5
+if num % 5 == 0:
+    total += 1
+
+# Ignore case of divisible by 5
+while sum <= num:
+    # Add four until creating num divisible by 5
+    sum += 4
+    if (num-sum) % 5 == 0:
+        total += 1
+
+print(total)
